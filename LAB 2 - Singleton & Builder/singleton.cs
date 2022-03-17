114ad5a3be6be94b1dac3b6e77ebdceef51d31f3
mmmyjak <84145031+mmmyjak@users.noreply.github.com>
2022-03-17 10:49:42 +0100

    Add files via upload

---
+++ LAB 2 - Singleton & Builder/singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace singleton
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton sin1 = Singleton.GetInstance();
+            Singleton sin2 = Singleton.GetInstance();
+            Console.WriteLine(sin1 == sin2);
+
+        }
+    }
+}
